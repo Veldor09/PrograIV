7d5d3a064580192cc31d325571e20ebb0ec8fd57
Veldor09 <arayavegad@gmail.com>
2025-02-27 23:08:21 -0600

    prueba3

---
+++ prueba3.js
@@ -0,0 +1,52 @@
+//
+const arr = [1, 2, 3];
+const [x, y] = arr;
+console.log(y);
+// respuesta correcta es "2"
+
+//
+let a = [1, 2, 3];
+let b = a;
+b.push(4);
+console.log(a);
+// respuesta correcta es "[1, 2, 3, 4]"
+
+//
+console.log(1 + '1' - 1);
+// respuesta correcta es "10"
+
+// 
+console.log(typeof NaN);
+// respuesta correcta es "number"
+
+//
+console.log(0 == '0');
+// respuesta correcta es "true"
+
+//
+let c = 5;
+let v = c++;
+console.log(v);
+// respuesta correcta es "5"
+
+//
+console.log(typeof null);
+// respuesta correcta es "object"
+
+//
+let n = {};
+let m = n;
+console.log(n === m);
+// respuesta correcta es "true"
+
+//
+console.log([] == false);
+// respuesta correcta es true
+
+//
+let g = 10;
+(function() {
+  console.log(g);
+  let g = 20;
+})();
+// la respuesta fue ReferenceError
